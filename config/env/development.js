71e12adba6ec620b551598ee033c0a3027d54dd4
Harry <harald.lurger@gmail.com>
2014-12-16 11:12:36 +0100

    update config

--- config/env/development.js
+++ config/env/development.js
@@ -18,7 +18,7 @@ module.exports = {
    ***************************************************************************/
 
   models: {
-    connection: 'sails-postgresql'
+    connection: 'localDiskDb'
   }
 
 };
